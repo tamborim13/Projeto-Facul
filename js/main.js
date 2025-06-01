0aa6404eab78d117982ebc0d1057333ca6b3414f
tamborim13 <tamborimgiovanni2017@gmail.com>
2025-06-01 14:24:10 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('whatsapp-btn').addEventListener('click', function () {
+  const linkDoGrupo = "https://chat.whatsapp.com/KMMBtNoL6ZNEgoh4JM2eBC";
+  window.location.href = linkDoGrupo;
+});
